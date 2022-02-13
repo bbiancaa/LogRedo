320d4d6029a1bc880569fffe564e159f3f3361f0
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 18:40:29 -0300

    apagando valores iniciais no arquivo conn

--- conn.py
+++ conn.py
@@ -27,7 +27,6 @@ def create_table(cursor):
     ''')
 
     cursor.execute("delete from at2;")
-    cursor.execute("insert into at2 values (1, 100, 20);")
-    cursor.execute("insert into at2 values (2, 20, 30);")
+    cursor.execute("insert into at2 values (1, null, null);")
+    cursor.execute("insert into at2 values (2, null, null);")
     cursor.execute("select * from at2;")
-    
