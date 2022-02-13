382e4f26b066c59b9dc4b00d73aa221c0e8fe379
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 18:18:49 -0300

    criação de função para ler arquivo

--- log.py
+++ log.py
@@ -1,11 +1,30 @@
-import psycopg2
 import conn
 
+def read_file():
+    values = []
+    commands = []
+    with open("./entrada.txt", "r") as file:
+        for line in file:
+            line = line.replace("\n", "")
+            line = line.strip()
+
+            if line == "":
+                continue
+
+            if line.startswith("<") and line.endswith(">"):
+                commands.append(line)
+            else:
+                values.append(line)
+    return (values, commands)
 
 def main():
     connection = conn.connect()
     cursor = connection.cursor()
     conn.create_table(cursor)
+
+    values, commands = read_file()
+    print(values, commands)
+
 
 if __name__ == '__main__':
     main()
