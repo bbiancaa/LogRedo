0faa0e6757444537433303bed77de337209788e9
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 18:48:47 -0300

    refatorando função de inserir valores

--- log.py
+++ log.py
@@ -1,7 +1,6 @@
 import conn
 
-def read_file():
-    values = []
+def read_file(cursor):
     commands = []
     with open("./entrada.txt", "r") as file:
         for line in file:
@@ -14,19 +13,15 @@ def read_file():
             if line.startswith("<") and line.endswith(">"):
                 commands.append(line)
             else:
-                values.append(line)
-    return values, commands
+                print('initial line: \t\t', line.replace(",", " ").replace("=", " ").split(" "))
+                column, id, value = line.replace(",", " ").replace("=", " ").split(" ")
+                cursor.execute("select * from at2")
+                print('before update \t\t', cursor.fetchall())
+                cursor.execute("update at2 set %s=%s where id=%s;" % (column, value, id))
+                cursor.execute("select * from at2")
+                print('after update \t\t', cursor.fetchall())
 
-
-def insert_values(cursor, values):
-    for val in values:
-        print('inicial line: \t\t', val.replace(",", " ").replace("=", " ").split(" "))
-        column, id, value = val.replace(",", " ").replace("=", " ").split(" ")
-        cursor.execute("select * from at2")
-        print('before update \t\t', cursor.fetchall())
-        cursor.execute("update at2 set %s=%s where id=%s;" % (column, value, id))
-        cursor.execute("select * from at2")
-        print('after update \t\t', cursor.fetchall())
+    return commands
 
 
 def main():
@@ -34,8 +29,7 @@ def main():
     cursor = connection.cursor()
     conn.create_table(cursor)
 
-    values, commands = read_file()
-    insert_values(cursor, values)
+    commands = read_file(cursor)
 
 
 if __name__ == '__main__':
