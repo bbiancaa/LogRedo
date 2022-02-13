173e988400f85397730fb1fff31d8aff53960f11
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 18:39:31 -0300

    inserindo valores iniciais do arquivo

--- log.py
+++ log.py
@@ -15,7 +15,19 @@ def read_file():
                 commands.append(line)
             else:
                 values.append(line)
-    return (values, commands)
+    return values, commands
+
+
+def insert_values(cursor, values):
+    for val in values:
+        print('inicial line: \t\t', val.replace(",", " ").replace("=", " ").split(" "))
+        column, id, value = val.replace(",", " ").replace("=", " ").split(" ")
+        cursor.execute("select * from at2")
+        print('before update \t\t', cursor.fetchall())
+        cursor.execute("update at2 set %s=%s where id=%s;" % (column, value, id))
+        cursor.execute("select * from at2")
+        print('after update \t\t', cursor.fetchall())
+
 
 def main():
     connection = conn.connect()
@@ -23,7 +35,7 @@ def main():
     conn.create_table(cursor)
 
     values, commands = read_file()
-    print(values, commands)
+    insert_values(cursor, values)
 
 
 if __name__ == '__main__':
