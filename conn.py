674beab3677f735397c38e2822ce7f253832438f
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 17:44:36 -0300

    criado arquivo para conexão e criação das tabelas

---
+++ conn.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+PG_DB = 'bd2'
+PG_USER = 'postgres'
+PG_PSWD = 'postgres'
+PG_HOST = 'localhost'
+PG_PORT = 5432
+
+
+def connect():
+    connection = psycopg2.connect(
+        database=PG_DB,
+        user=PG_USER,
+        password=PG_PSWD,
+        host=PG_HOST,
+        port=PG_PORT,
+    )
+    return connection
+
+def create_table(cursor):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS at2 (
+            Id INT PRIMARY KEY,
+            A INT,
+            B INT
+        );
+    ''')
+
+    cursor.execute("insert into at2 values (1, null, null);")
+    cursor.execute("insert into at2 values (2, null, null);")
+    cursor.execute("select * from at2;")
+    print(cursor.fetchall())
+    
--- log.py
+++ log.py
@@ -1,21 +1,11 @@
-from config import config
 import psycopg2
+import conn
+
 
 def main():
-    params = config()
-    # conectando no banco
-    conn = psycopg2.connect(**params)
-    # instanciando objeto para consultas
-    cur = conn.cursor()
-    #realizando consulta
-    cur.execute('SELECT * from redo')
-    #pegando resultado 
-    result = cur.fetchone()
-    #printando resultado
-    print(result)
-    #fechando conexao com o banco
-    cur.close()
-
+    connection = conn.connect()
+    cursor = connection.cursor()
+    conn.create_table(cursor)
 
 if __name__ == '__main__':
     main()
