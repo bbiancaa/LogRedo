3fea3af80a3cd8bd3bb2b4b3a9a0dbc841be536f
bianca <biancagabriela.fritsch@hotmail.com>
2022-02-13 21:03:33 -0300

    inserindo commits no banco

--- log.py
+++ log.py
@@ -1,11 +1,44 @@
 import conn
 
+
+def beautiful_str(str, value):
+    return str.replace('<', '').replace('>', '').replace(value, '').strip()
+
+
+def process_commands(cursor, values):
+    start = {
+
+    }
+    checkpoint = {
+
+    }
+    for value in values:
+        if 'Start CKPT' in value:
+            checkpoint[beautiful_str(value, 'Start CKPT')] = []
+        elif 'start' in value:
+            start[beautiful_str(value, 'start')] = []
+        elif 'commit' in value:
+            position = beautiful_str(value, 'commit')
+            for commit in reversed(start[position]):
+                id, column, val = commit.split(',')
+                cursor.execute(f"update at2 set {column}={val} where id={id};")
+                start[position].pop(0)
+
+        elif 'End CKPT' in value:
+            pass
+        elif 'crash' in value:
+            pass
+        else:
+            position = beautiful_str(value, '').split(',')[0]
+            start[position].append(beautiful_str(value, position+','))
+
+    print(start, checkpoint)
+    
 def read_file(cursor):
     commands = []
     with open("./entrada.txt", "r") as file:
         for line in file:
-            line = line.replace("\n", "")
-            line = line.strip()
+            line = line.replace("\n", "").strip()
 
             if line == "":
                 continue
@@ -30,6 +63,7 @@ def main():
     conn.create_table(cursor)
 
     commands = read_file(cursor)
+    process_commands(cursor, commands)
 
 
 if __name__ == '__main__':
